0d3fff39a5b6631ca0b763f9b6c79981a29bc6ea
Student <Student@Makerss-MacBook-Air-2.local>
2020-02-24 15:18:47 +0000

    Passed test for topup method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 class Oystercard
-    attr_reader :balance
-    def initialize(balance = 0)
-        @balance = balance
-    end
+  attr_reader :balance
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
+  def top_up(value)
+    @balance += value
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 require 'oystercard'
 describe Oystercard do
-    it "check if balance by default is 0" do
-        expect(subject.balance).to eq 0
+  it 'check if balance by default is 0' do
+    expect(subject.balance).to eq 0
+  end
+  describe '#top_up' do
+    it "top up's the balance with the value" do
+      expect(subject.top_up(5)).to eq 5
     end
+  end
 end
